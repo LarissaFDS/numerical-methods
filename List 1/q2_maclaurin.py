9db6bfaf1113078921fd4b0d979d35778cf26160
Larissa <larissafdds@gmail.com>
2026-09-18 22:11:31 -0300

    update q2

--- List 1/q2_maclaurin.py
+++ List 1/q2_maclaurin.py
@@ -3,6 +3,37 @@ from decimal import Decimal, localcontext, ROUND_HALF_EVEN
 K4 = 4
 ALTA = 40
 
+class ContaDecimal:
+    contador = 0
+
+    def __init__(self, valor):
+        self.valor = valor if isinstance(valor, Decimal) else Decimal(valor)
+
+    def _bin_op(self, outro, func):
+        ContaDecimal.contador += 1
+        outro_val = outro.valor if isinstance(outro, ContaDecimal) else Decimal(outro)
+        return ContaDecimal(func(self.valor, outro_val))
+
+    def __mul__(self, outro):
+        return self._bin_op(outro, lambda a, b: a * b)
+    __rmul__ = __mul__
+
+    def __add__(self, outro):
+        return self._bin_op(outro, lambda a, b: a + b)
+    __radd__ = __add__
+
+    def __sub__(self, outro):
+        return self._bin_op(outro, lambda a, b: a - b)
+
+    def __rsub__(self, outro):
+        ContaDecimal.contador += 1
+        outro_val = outro.valor if isinstance(outro, ContaDecimal) else Decimal(outro)
+        return ContaDecimal(outro_val - self.valor)
+
+    def __repr__(self):
+        return repr(self.valor)
+
+
 def arctan(x):
     with localcontext() as ctx:
         ctx.prec = ALTA
@@ -11,54 +42,42 @@ def arctan(x):
         x2 = x * x
         sinal = 1
         n = 1
-
         for _ in range(200):
             termo = pot / Decimal(n)
-
             if sinal > 0:
                 s += termo
             else:
                 s -= termo
-
             if abs(termo) < Decimal("1e-35"):
                 break
-
             pot *= x2
             n += 2
             sinal *= -1
-
         return s
 
 
 def calcular_pi():
     with localcontext() as ctx:
         ctx.prec = ALTA
-
         a = arctan(Decimal("1") / Decimal("5"))
         b = arctan(Decimal("1") / Decimal("239"))
-
         return Decimal("16") * a - Decimal("4") * b
 
 
 def referencia_cosseno(x):
     with localcontext() as ctx:
         ctx.prec = ALTA
-
         x = Decimal(x)
         termo = Decimal("1")
         soma = Decimal("1")
         n = 1
-
         for _ in range(50):
-            # Série de Taylor para o cosseno: 1 - x^2/2! + x^4/4! - ...
+            # Série de Taylor para cosseno: 1 - x^2/2! + x^4/4! - ...
             termo = -termo * x * x / Decimal((2*n-1)*(2*n))
             soma += termo
-
             if abs(termo) < Decimal("1e-35"):
                 break
-
             n += 1
-
         return soma
 
 
@@ -66,7 +85,6 @@ def coeficientes():
     with localcontext() as ctx:
         ctx.prec = K4
         ctx.rounding = ROUND_HALF_EVEN
-
         return (
             Decimal("1") / Decimal("2"),
             Decimal("1") / Decimal("24"),
@@ -83,20 +101,18 @@ def nao_aninhado(z):
         ctx.prec = K4
         ctx.rounding = ROUND_HALF_EVEN
 
-        z2 = z * z
+        ContaDecimal.contador = 0
+        zc = ContaDecimal(z)
+        Ac, Bc, Cc, Dc = ContaDecimal(A), ContaDecimal(B), ContaDecimal(C), ContaDecimal(D)
+
+        z2 = zc * zc
         z4 = z2 * z2
         z6 = z4 * z2
         z8 = z4 * z4
 
-        resultado = (
-            Decimal("1")
-            - z2 * A
-            + z4 * B
-            - z6 * C
-            + z8 * D
-        )
+        resultado = ContaDecimal(Decimal("1")) - z2 * Ac + z4 * Bc - z6 * Cc + z8 * Dc
 
-        return resultado, 12
+        return resultado.valor, ContaDecimal.contador
 
 
 def aninhado(z):
@@ -104,22 +120,17 @@ def aninhado(z):
         ctx.prec = K4
         ctx.rounding = ROUND_HALF_EVEN
 
-        y = z * z
-        resultado = (
-            Decimal("1")
-            + y * (
-                -A
-                + y * (
-                    B
-                    + y * (
-                        -C
-                        + y * D
-                    )
-                )
-            )
-        )
+        ContaDecimal.contador = 0
+        y = ContaDecimal(z) * ContaDecimal(z)
 
-        return resultado, 9
+        # Coeficientes na ordem do maior grau (em y) até o termo constante
+        coefs_horner = [D, -C, B, -A, Decimal("1")]
+
+        resultado = ContaDecimal(coefs_horner[0])
+        for c in coefs_horner[1:]:
+            resultado = resultado * y + ContaDecimal(c)
+
+        return resultado.valor, ContaDecimal.contador
 
 
 if __name__ == "__main__":
